df58423374e34eb634defe336e13dd4f3fe83785
xrplong <57891184+xrplong@users.noreply.github.com>
2020-12-30 03:36:40 +1100

    Update Sending team&year data to new folders.py

--- Sending team&year data to new folders.py
+++ Sending team&year data to new folders.py
@@ -1,6 +1,6 @@
 teams = ['Baltimore Orioles', 'Boston Red Sox', 'New York Yankees', 'Tampa Bay Rays', 'Toronto Blue Jays',
          'Chicago White Sox', 'Cleveland Indians', 'Detroit Tigers', 'Kansas City Royals', 'Minnesota Twins',
-         'Houston Astros', 'Los Angeles Angles', 'Oakland Athletics', 'Seattle Mariners', 'Texas Rangers',
+         'Houston Astros', 'Los Angeles Angels', 'Oakland Athletics', 'Seattle Mariners', 'Texas Rangers',
          'Atlanta Braves', 'Miami Marlins', 'New York Mets', 'Philadelphia Phillies', 'Washington Nationals',
          'Chicago Cubs', 'Cincinnati Reds', 'Milwaukee Brewers', 'Pittsburgh Pirates', 'St.Louis Cardinals',
          'Arizona Diamondbacks', 'Colorado Rockies', 'Los Angeles Dodgers', 'San Diego Padres', 'San Francisco Giants']
